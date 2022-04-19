969b98953a014ce51e84b140f255c05a86033eb0
lalalalyt <lynneli318@gmail.com>
2022-04-19 11:39:15 -0400

    finish tourney-matches

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,11 +1,23 @@
 import Match from "./Match";
-import matchData from "../data/matchData"
+import matchData from "../data/matchData";
 
-function MatchList() {
-  <section className="PlayerList MatchList">
-    <h1>Match list</h1>
-    <Match />
-  </section>;
+function MatchList(props) {
+  const matches = matchData.map((match) => {
+    return (
+      <Match
+        key = {match.matchNumber}
+        players={match.players}
+        winner={match.winner}
+        scoreDifference={match.scoreDifference}
+      />
+    );
+  });
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {matches}
+    </section>
+  );
 }
 
 export default MatchList;
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,14 +1,27 @@
 import Player from "./Player";
 import playerData from "../data/playerData";
 import matchData from "../data/matchData";
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
-function PlayerList() {
-  return (
-    <section className="PlayerList">
-      <h1>Current participating players</h1>
-      <Player />
-    </section>
-  );
+function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const players = parsedPlayerData.map((player) => {
+    return (
+        <Player
+          key={player.gamerTag}
+          gamerTag={player.gamerTag}
+          firstName={player.firstName}
+          lastName={player.lastName}
+          wins={player.wins}
+        />)
+    });
+    return (
+      <section className="PlayerList">
+        <h1>Current participating players</h1>
+        {players}
+      </section>
+    );
 }
 
 export default PlayerList;
